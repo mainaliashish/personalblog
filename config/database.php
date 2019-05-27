0d0e6c1af9a9fb617f0b123e4d57daf8e41ed3a5
ashishmainalee <ashishmainalee@gmail.com>
2019-05-27 19:57:00 +0545

    Category Fix

--- config/database.php
+++ config/database.php
@@ -45,12 +45,12 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'url' => env('DATABASE_URL'),
+            'url' => env('db4free.net'),
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'personalblog'),
+            'username' => env('DB_USERNAME', 'personalblog'),
+            'password' => env('DB_PASSWORD', 'ashishmainalee12345'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
